8f6d2a794ac1876a82f48f68f30f703641b4402c
Albert <albert@DESKTOP-43VQ7BP.localdomain>
2022-07-01 16:18:20 +0800

    add replacedb function

--- newInstallLEMP.sh
+++ newInstallLEMP.sh
@@ -7,8 +7,8 @@
 # Optional args?  if not specified install everything.
 #     -c [web_domain] [web_email]    generate TLS/SSL certificate and install
 #     -d [mysql rootpass] [mysql database] [mysqluser] [mysql pass] [db file] TODO: check if database exists 
-#     dev env.(ipv4 address) versus prod env.(domain name):  make backup of old database and replace with new one
-#     -rd [database name ] [db file]
+#     feature: dev env.(ipv4 address) versus prod env.(domain name):  make backup of old database and replace with new one
+#     -R [mysql rootpass] [database name ] [db file|new domain name or ipaddress] 
 #
 
 
@@ -77,6 +77,103 @@ else # ONLY WHEN NECESSARY: re-install because a main setting (ie: ipv4 address 
 fi
 }
 
+newdb(){
+# Installing MariabDB Server
+sudo apt install -y mariadb-server
+# Starting MariaDB Service
+sudo systemctl start mariadb
+# Enabling MariaDB service so that it keeps up and running during restarts
+sudo systemctl enable mariadb
+# Installing expect tool to work with interactive commands
+sudo apt install -y expect
+
+# Automating the Database configuration
+MYSQL_ROOT_PASSWORD=${args[mysqlrootpass]}
+
+SECURE_MYSQL=$(expect -c "
+set timeout 3
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+expect \"Switch to unix_socket authentication\"
+send \"y\r\"
+expect \"Change the root password?\"
+send \"n\r\"
+expect \"New password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+expect \"Re-enter new password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+# Add check to see if database exists?  Check already exists.
+# Create new database and new database user for WordPress
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create or replace database '${args[mysqldb]}';
+show warnings;
+create user '${args[mysqluser]}'@'localhost' identified by '${args[mysqlpass]}';
+grant all privileges on '${args[mysqldb]}'.* to '${args[mysqluser]}'@'localhost';
+flush privileges;
+exit;"
+
+}
+
+# Replace current WordPress database with a new one
+# Feature: Use new web address or IP if specified (ie: for wordpress dev. environment)
+# 
+# Usage:    replacedb [mysql rootpass] [database name ] [old domain or ip] [db file|new domain name or ipaddress]
+#         or replacedb [mysql rootpass] [database name] [new sql database]
+replacedb(){
+
+# backup old database to dbs.sql
+echo "make backup of old WP database in /var/www/html/dbs.sql.bak" 
+mysqldump -u root -p$1 -x -B $2 > /var/www/html/dbs.sql
+local OLDADDRESS=$3
+local NEWADDRESS=$4
+
+echo "$OLDADDRESS"
+echo "$NEWADDRESS"
+
+# if database file is specified then 
+# TODO: use reqex to determine if $3 argument is a sql database
+if [[ $3 -eq '*\.sql' ]]; then
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create or replace database '$3';
+show warnings;
+exit;"
+
+else # ipaddress or domain name specified
+
+# sed -i.bak -e "s/gmental\.com/54\.241\.45\.216/g w output2.txt" db.sql
+# grep -o '54.241.45.216' db.sql | wc -l
+
+  echo "count of old addresses in database file"
+  grep -o '$OLDADDRESS' dbs.sql | wc -l
+  sed -i.bak -e "s/$OLDADDRESS/$NEWADDRESS/g w output.txt" dbs.sql
+  echo "count of new addresses in database file"
+  grep -o '$NEWADDRESS' dbs.sql | wc -l
+
+fi
+}
+
+
+#    mysql -u root -p$1 -e "create or replace database '$2';
+#    show warnings;
+#    exit;"
+
+
+
+
+
 main() {
 
 # Check number of arguments
@@ -100,7 +197,7 @@ elif [[ "${ARGV[0]}" == "-d" && $ARGS -ge 4 ]]; then
 	echo "${ARGV[0]}"
 	echo "create a new database with specified arguments"
 
-# Replace the current database with specified sql file
+# Replace the current database with specified sql file or replace current database with new ip/domain
 elif [[ "${ARGV[0]}" == '-R' && $ARGS -eq 2 ]]; then
 	echo "${ARGV[0]}"
 	echo "replace the current database with specified sql file"
